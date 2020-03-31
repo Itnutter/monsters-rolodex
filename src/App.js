4ed74cb4a8b3dd1321406d2399b23e1bfa2e603b
Riaz Ahmed <riaz.ahmed@systemsltd.com>
2020-03-31 12:10:29 +0500

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Class decorator allow us to declare state of the application
+
+class App extends Component {
+  // create constructor
+  constructor() {
+    // call the parent constructor
+    super();
+
+    // set the base state
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+  }
+
+  // componentDidMount() is invoked immediately after a component is mounted (inserted into the tree)
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }))
+  }
+
+  handleChange = e => {
+    this.setState({ searchField: e.target.value })
+  }
+
+  render() {
+
+    // destructuring
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+      )
+
+    return (
+      <div className="App">
+
+        <h1>Monsters Rolodex</h1>
+
+        {/* Resuable searchbox component */}
+        <SearchBox
+          placeholder='search monsters'
+          handleChange={this.handleChange}
+        />
+
+        {/* Filtered array list pass down into CardList Component for search functionality */}
+        <CardList monsters={filteredMonsters} />
+          {/* Write JavaScript using {} brackets */}
+          {/*{
+            this.state.monsters.map(
+              monster => <h1 key={monster.id}>{ monster.name }</h1>
+            )
+          }
+        </CardList>*/}
+      </div>
+    )
+  }
 }
+
 
 export default App;
