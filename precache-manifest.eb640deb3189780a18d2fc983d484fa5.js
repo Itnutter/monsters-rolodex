04294ed8e4ad022c33ad424563eca6a346264c7f
Riaz Ahmed <riaz.ahmed@systemsltd.com>
2020-03-31 12:09:15 +0500

    Updates

---
+++ precache-manifest.eb640deb3189780a18d2fc983d484fa5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "745fc2c50510984077a55bed3f32ec9d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ce6fc79071ff0202cc60",
+    "url": "/monsters-rolodex/static/css/main.4a481fc3.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce6fc79071ff0202cc60",
+    "url": "/monsters-rolodex/static/js/main.bba881d5.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
